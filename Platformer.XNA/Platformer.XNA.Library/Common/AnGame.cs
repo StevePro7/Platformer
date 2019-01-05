e0bc842dce95808c71118efb677063816203d378
StevePro7 <steveprozune@hotmail.com>
2019-01-05 20:44:26 +0000

    Update projects

---
+++ Platformer.XNA/Platformer.XNA.Library/Common/AnGame.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace WindowsGame.Common
+{
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	public class AnGame : Game
+	{
+		private readonly GraphicsDeviceManager graphics;
+
+		public AnGame()
+		{
+			graphics = new GraphicsDeviceManager(this) { SupportedOrientations = DisplayOrientation.LandscapeLeft };
+		}
+
+		protected override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			base.LoadContent();
+		}
+
+		protected override void UnloadContent()
+		{
+			base.UnloadContent();
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
+		}
+
+		protected override void OnActivated(object sender, EventArgs args)
+		{
+			base.OnActivated(sender, args);
+		}
+
+		protected override void OnDeactivated(object sender, EventArgs args)
+		{
+			base.OnDeactivated(sender, args);
+		}
+
+		protected override void OnExiting(object sender, EventArgs args)
+		{
+			base.OnExiting(sender, args);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Platformer
 Placeholder repo for Platformer
+
+
+Install-Package Ninject -Version 3.2.2.0
+packages\Ninject.3.2.2.0\lib\net40\Ninject.dll
+
+Install-Package NUnit -Version 3.2.1
+packages\NUnit.3.2.1\lib\net40\nunit.framework.dll
