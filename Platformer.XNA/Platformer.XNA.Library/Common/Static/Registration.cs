a2b27a7bd99b27b1b229247945b3d7b4b20fb278
StevePro7 <steveprozune@hotmail.com>
2019-01-05 20:51:42 +0000

    Add Common folder

---
+++ Platformer.XNA/Platformer.XNA.Library/Common/Static/Registration.cs
@@ -0,0 +1,19 @@
+﻿using WindowsGame.Common.Managers;
+using WindowsGame.Common.TheGame;
+using WindowsGame.Common.Implementation;
+using WindowsGame.Common.Interfaces;
+using WindowsGame.Common.IoC;
+
+namespace WindowsGame.Common.Static
+{
+	public static class Registration
+	{
+		public static void Initialize()
+		{
+			IoCContainer.Initialize<IGameManager, GameManager>();
+
+			IoCContainer.Initialize<IFileProxy, ProdFileProxy>();
+			IoCContainer.Initialize<IFileManager, FileManager>();
+		}
+	}
+}
---
+++ Platformer.XNA/Platformer.XNA.Library/Common/TheGame/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WindowsGame.Common.Interfaces;
+using WindowsGame.Common.Managers;
+
+namespace WindowsGame.Common.TheGame
+{
+	public interface IGameManager
+	{
+		IFileManager FileManager { get; }
+		ILogger Logger { get; }
+	}
+
+	public class GameManager : IGameManager
+	{
+		public GameManager
+		(
+			IFileManager fileManager,
+			ILogger logger
+		)
+		{
+			FileManager = fileManager;
+			Logger = logger;
+		}
+
+		public IFileManager FileManager { get; private set; }
+		public ILogger Logger { get; private set; }
+	}
+}
