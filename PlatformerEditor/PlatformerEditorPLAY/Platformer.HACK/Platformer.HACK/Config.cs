f5a853ee5fd31548b1c0410400033d45af87fdee
StevePro7 <steveprozune@hotmail.com>
2019-03-21 13:08:34 +0000

    Clone new editor

---
+++ PlatformerEditor/PlatformerEditorPLAY/Platformer.HACK/Platformer.HACK/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platformer
+{
+	public class Config
+	{
+		public Config(bool invincibility, bool optionalBlock)
+		{
+			Invincibility = invincibility;
+			OptionalBlock = optionalBlock;
+		}
+
+		public bool Invincibility { get; private set; }
+		public bool OptionalBlock { get; private set; }
+	}
+}
