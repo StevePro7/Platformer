cc210a83f75ba2afd44e3700adbdff2f589f9e3f
StevePro7 <steveprozune@hotmail.com>
2022-11-03 18:46:47 +0000

    no message

---
+++ Platformer.HACK_06_VS2015/Platformer.HACK_06b/EnumManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platformer
+{
+	public enum enum_move_type
+	{
+		move_type_left = 0,
+		move_type_idle = 1,
+		move_type_rght = 2,
+	}
+}
